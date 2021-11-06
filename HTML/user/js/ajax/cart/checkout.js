a4bd6eb29d29187c0ae76399b3efe2c729897d7a
x-Beo-x <86937244+x-Beo-x@users.noreply.github.com>
2021-11-06 16:56:45 +0700

    add checkout reference:

--- HTML/user/js/ajax/cart/checkout.js
+++ HTML/user/js/ajax/cart/checkout.js
@@ -1,4 +1,4 @@
-
+let baseUrl = "http://localhost:8080"
 
 function showCartDetail(){
     let url = `${baseUrl}/users/cart`
@@ -8,9 +8,12 @@ function showCartDetail(){
         headers: {
             "Authorization": "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdWFueWI5ODEiLCJpYXQiOjE2MzYxNjU5ODQsImV4cCI6ODgwMzYxNjU5ODR9.dtTA4i_YP2P-cCWenHQsS-EWlRvogsKWcdV4BCIvLLAHKrrANwrMb2dEWk06q5RLjBsXhKKMDmBzJX_8K0GPCg"
         },
-        success: function (){
-
+        success: function (data){
+            console.log(data)
+            drawCartDetail(data)
         }
+    }).fail(function (){
+        console.log('fail')
     })
 }
 
@@ -18,6 +21,53 @@ function drawCartDetail(data){
     let content = "";
     let estimatedPayment = 0;
     for (let i = 0; i < data.length; i++) {
-
+        var element = data[i];
+        var product = element.product;
+        var price = product.price*(1 - product.saleOff/100)
+        estimatedPayment += price * element.quantity
+        content += `
+                                             <tr>
+                                                <td>
+                                                    <h6 class="order-h6">${product.name}</h6>
+                                                    <span class="order-span-quantity">x 1</span>
+                                                </td>
+                                                <td>
+                                                    <h6 class="order-h6">${price}</h6>
+                                                </td>
+                                            </tr>`
     }
+
+    content +=                              `<tr>
+                                                <td>
+                                                    <h3 class="order-h3">Subtotal</h3>
+                                                </td>
+                                                <td>
+                                                    <h3 class="order-h3">$${estimatedPayment}</h3>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <h3 class="order-h3">Shipping</h3>
+                                                </td>
+                                                <td>
+                                                    <h3 class="order-h3">$0.00</h3>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <h3 class="order-h3">Tax</h3>
+                                                </td>
+                                                <td>
+                                                    <h3 class="order-h3">$0.00</h3>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <h3 class="order-h3">Total</h3>
+                                                </td>
+                                                <td>
+                                                    <h3 class="order-h3">$${estimatedPayment}</h3>
+                                                </td>
+                                            </tr>`
+    $("#cart").html(content)
 }

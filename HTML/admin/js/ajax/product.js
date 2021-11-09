a6fc99af3ded1725553369802bb6275faa8bc781
ngoxuanhuyxuyxa <86957583+ngoxuanhuyxuyxa@users.noreply.github.com>
2021-11-09 14:39:50 +0700

    update

--- HTML/admin/js/ajax/product.js
+++ HTML/admin/js/ajax/product.js
@@ -20,7 +20,6 @@ function showProduct() {
         url: 'http://localhost:8080/products',
         method: 'GET',
         success: function (data) {
-            console.log(data)
             let content = `<tr>
                         <th>ID</th>
                         <th>Name</th>
@@ -37,26 +36,31 @@ function showProduct() {
                 content += showById(data.content[i])
             }
 
-            document.getElementById("modalbutton").innerHTML = '<button id="modalbutton"  onclick="createNewDeliverFirm()" type="button" class="btn btn-success" \n' +
+            document.getElementById("modalbutton").innerHTML = `<button id="modalbutton"  onclick="createNewDeliverFirm()" type="button" class="btn btn-success" \n +
                 '                                                       data-bs-dismiss="modal">\n' +
-                '                                                      create</button>'
+                                                                      create</button>`
 
             document.getElementById("list").innerHTML = content
         }
     })
 }
 
-$(document).ready(function (){
-    $("#create-button").on('click', function (){
-        document.getElementById("modalbutton").innerHTML = '<button id="modalbutton"  onclick="createNewDeliverFirm()" type="button" class="btn btn-success" \n' +
-            '                                                       data-bs-dismiss="modal">\n' +
-            '                                                      create</button>'
+$(document).ready(function () {
+    $("#create-button").on('click', function () {
+        document.getElementById("modalbutton").innerHTML = `<button id="modalbutton"  onclick="createNewDeliverFirm()" type="button" class="btn btn-success"
+            '                                                      data-bs-dismiss="modal">\n
+            '                                                      create</button>`
     })
 })
+// $(document).ready(function (){
+//     $("#search").on('click',function (){
+//         searchByNameabc()
+//     })
+// })
 
 function showById(product) {
     return `<tr>
-            <td>${product.id}</td>
+           
             <td>${product.name}</td>
             <td>${product.quantity}</td>
             <td>${product.price}</td>
@@ -70,7 +74,7 @@ function showById(product) {
             <button type="button" onclick="showInfo(${product.id})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 update
             </button>
-            <button>delete</button>
+            <button type="button" onclick="deleteById(${product.id})">delete</button>
             </td>
           </tr>`
 }
@@ -86,23 +90,24 @@ function showInfo(id) {
             $('#quantity').val(data.quantity)
             $('#price').val(data.price)
             $('#saleOff').val(data.saleOff)
-            // $('#mainImage').val(data.mainImage)
-            // $('#subImage').val(data.subImage)
+            $('#mainImage').val(data.mainImage)
+            $('#subImage').val(data.subImage)
             $('#brand').val(data.brand.name)
             $('#description').val(data.description)
             $('#category').val(data.category.name)
-            document.getElementById("modalbutton").innerHTML = '<button id="modalbutton"  onclick="update(${product.id})" type="button" class="btn btn-success" \n' +
-                '                                    data-bs-dismiss="modal">\n' +
-                '                                   update</button>'
+            document.getElementById("modalbutton").innerHTML = `<button id="modalbutton"  onclick="update(${data.id})" type="button" class="btn btn-success" \n' +
+                '                                    data-bs-dismiss="modal">
+                '                                   update</button>`
         }
     })
 }
 
-function update() {
-    console.log(id)
+function update(id) {
+    let form = new FormData($("#create")[0])
+    let url = `http://localhost:8080/products/${id}`
     $.ajax({
-        url: 'http://localhost:8080/products' + `/` + id,
-        type:'PUT',
+        url: url,
+        type: 'PUT',
         enctype: 'multipart/form-data',
         processData: false,
         contentType: false,
@@ -111,6 +116,44 @@ function update() {
         data: form,
         success: function () {
             showProduct()
+        }
+    })
+}
+
+function deleteById(id){
+    $.ajax({
+        url:'http://localhost:8080/products'+`/`+id,
+        type:'DELETE',
+        success:function (){
+            showProduct()
+        }
+    })
+}
+
+function searchByNameabc(){
+   let  name = $('#search').val()
+    $.ajax({
+        url:'http://localhost:8080/products'+`?`+`q=`+name,
+        type:'GET',
+        success:function (data){
+            console.log(data)
+            let content = `<tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>SaleOff</th>
+                        <th>MainImage</th>
+                        <th>SubImage</th>
+                        <th>Brand</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                    </tr>`
+            for (let i = 0; i < data.content.length; i++) {
+                content += showById(data[i])
+            }
+            console.log(content)
+            document.getElementById("list").innerHTML=content
         }
     })
 }
